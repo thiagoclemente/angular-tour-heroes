6eb73893b7b2dc30ba531d88b2d8cefe1157ff91
Thiago Clemente <thiago.clemente@gmail.com>
2020-05-20 21:05:27 -0300

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Hero 11' },
+  { id: 12, name: 'Hero 12' },
+  { id: 13, name: 'Hero 13' },
+  { id: 14, name: 'Hero 14' },
+  { id: 15, name: 'Hero 15' },
+  { id: 16, name: 'Hero 16' },
+  { id: 17, name: 'Hero 17' },
+  { id: 18, name: 'Hero 18' },
+  { id: 19, name: 'Hero 19' },
+  { id: 20, name: 'Hero 20' }
+];
